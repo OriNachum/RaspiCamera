f431971a99f3f3e08bff196adbcf1d73917d2331
OriNachum <ori.nachum@gmail.com>
2019-12-20 13:28:03 +0200

    Added python libraries for camera and time

--- RaspiCamera/python.TakePicture.py
+++ RaspiCamera/python.TakePicture.py
@@ -1,4 +1,7 @@
-﻿camera.start_preview()
+﻿from picamera import PiCamera
+from time import sleep
+
+camera.start_preview()
 sleep(5)
 camera.capture('/home/pi/PythonPicture.jpg')
 camera.stop_preview()
